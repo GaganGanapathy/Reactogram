0b0edebb6043308008d752f8553257058a90c464
Gagan Ganapathy <109824692+GaganGanapathy@users.noreply.github.com>
2023-10-12 02:16:59 +0530

    Update config.js

--- Reactogram_be/config.js
+++ Reactogram_be/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGO_DB_URL: "mongodb://0.0.0.0:27017/usermodels",
+  MONGO_DB_URL: "mongodb+srv://damiandesmond1020:RsyJPZfhthP2RYX2@cluster0.bgcz4ry.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET:"skjdbfsdf287t3jkasftasfasfha87"
 }
