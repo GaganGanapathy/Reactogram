03c0a26c5a1895931654a323325c1251315e31b3
Gagan <ganapathi.gagan@gmail.com>
2023-11-05 22:03:35 +0530

    deploy

--- Reactogram_be/config.js
+++ Reactogram_be/config.js
@@ -1,10 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-  MONGO_DB_URL: "mongodb+srv://damiandesmond1020:RsyJPZfhthP2RYX2@cluster0.bgcz4ry.mongodb.net/?retryWrites=true&w=majority",
-  JWT_SECRET:"skjdbfsdf287t3jkasftasfasfha87"
-=======
   MONGO_DB_URL:
     "mongodb+srv://damiandesmond1020:RsyJPZfhthP2RYX2@cluster0.bgcz4ry.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "skjdbfsdf287t3jkasftasfasfha87",
->>>>>>> 360d66e (url)
 }
