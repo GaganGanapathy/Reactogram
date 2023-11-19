258e083ad3bd852252ea42e30a59580328b92bb1
Gagan <ganapathi.gagan@gmail.com>
2023-11-19 13:39:12 +0530

    code cleanup

--- Reactogram_fe/vite.config.js
+++ Reactogram_fe/vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "/path/to/main.js",
-    },
-  },
 })
